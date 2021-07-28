4ca70385c1de96d57b0749236f92409edb613263
Dave Cottlehuber <dch@skunkwerks.at>
2021-07-28 21:00:13 +0000

    use new CURRENT image from andy

--- main.tf
+++ main.tf
@@ -6,7 +6,23 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		is_monitoring_disabled = "true"
 		plugins_config {
 			desired_state = "DISABLED"
+			name = "Vulnerability Scanning"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute Instance Run Command"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
 			name = "Compute Instance Monitoring"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Block Volume Management"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Bastion"
 		}
 	}
 	availability_config {
@@ -24,15 +40,15 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		are_legacy_imds_endpoints_disabled = "false"
 	}
 	metadata = {
-		"ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ0cNlRkFRRleUZhFjIZYJ2p7h7wNWvODGBLEzfSfvr dch@skunkwerks.at 20201124"
+		"ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ0cNlRkFRRleUZhFjIZYJ2p7h7wNWvODGBLEzfSfvr dch@skunkwerks.at 20201124\necdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBItIwUr8zhXOBtFH1B0YmNz2WJcY6w1ysRiTAIkI2CBenMb0f7H2pH1rFAa6ZF6dYS3SuLMng+igZUfkqhV/0Km+zus3lAjc37FFiawtATt+/nRj3hj/AaVz/cK7NnWdlg== dch@yk10616337_eccp384_20191024"
 	}
 	shape = "VM.Standard.A1.Flex"
 	shape_config {
-		memory_in_gbs = "4"
-		ocpus = "1"
+		memory_in_gbs = "24"
+		ocpus = "4"
 	}
 	source_details {
-		source_id = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaabmpqimzufiv6an4qq6qan5k6xwwiewgmtlnzdg5uj264l4hmpu6a"
+		source_id = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaaajpkvg6f3hoqfhwldwzzc3pfp6smizimjo5cpk7phufadhryhsaa"
 		source_type = "image"
 	}
 }
