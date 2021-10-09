4c6f236e63b6f734e795c2f40c8c67a7225e3967
Dave Cottlehuber <dch@skunkwerks.at>
2021-10-09 15:58:45 +0000

    switch a1-flex to boot mfs

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "oci" {}
 
-resource "oci_core_instance" "generated_oci_core_instance" {
+resource "oci_core_instance" "bilbo" {
 	agent_config {
 		is_management_disabled = "true"
 		is_monitoring_disabled = "true"
@@ -35,12 +35,12 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		assign_public_ip = "true"
 		subnet_id = "ocid1.subnet.oc1.eu-amsterdam-1.aaaaaaaa7lfgj4fcyfxlqc7cr4nhottzufvo5zzj2vc4syayk23i4rchenfq"
 	}
-	display_name = "beastie"
+	display_name = "bilbo.cabal5.net"
 	instance_options {
 		are_legacy_imds_endpoints_disabled = "false"
 	}
 	metadata = {
-		"ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ0cNlRkFRRleUZhFjIZYJ2p7h7wNWvODGBLEzfSfvr dch@skunkwerks.at 20201124\necdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBItIwUr8zhXOBtFH1B0YmNz2WJcY6w1ysRiTAIkI2CBenMb0f7H2pH1rFAa6ZF6dYS3SuLMng+igZUfkqhV/0Km+zus3lAjc37FFiawtATt+/nRj3hj/AaVz/cK7NnWdlg== dch@yk10616337_eccp384_20191024"
+		"ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHuuKqa0KEiCFC3Pr5LmWae/ZZfxOQcH9b9jFHLEMC3t ansible@cabal5.net 20201223\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ0cNlRkFRRleUZhFjIZYJ2p7h7wNWvODGBLEzfSfvr dch@skunkwerks.at 20201124\necdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBItIwUr8zhXOBtFH1B0YmNz2WJcY6w1ysRiTAIkI2CBenMb0f7H2pH1rFAa6ZF6dYS3SuLMng+igZUfkqhV/0Km+zus3lAjc37FFiawtATt+/nRj3hj/AaVz/cK7NnWdlg== dch@yk10616337_eccp384_20191024"
 	}
 	shape = "VM.Standard.A1.Flex"
 	shape_config {
@@ -48,7 +48,66 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		ocpus = "4"
 	}
 	source_details {
-		source_id = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaaajpkvg6f3hoqfhwldwzzc3pfp6smizimjo5cpk7phufadhryhsaa"
 		source_type = "image"
+	    source_id = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaaevm6ugkqooromkcisry4fhr6frfat6kjmkpwrw3q4uzzzkue6fzq"
+		/* ipxe efifat */
+		/* source_id = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaaprit3bcx5i6oxmy22intyavttmqcnrttc4rqkiqd4is2q24ym34a" */
 	}
+
+    /* ipxe_script = <<-IPXE */
+    /*     #!ipxe */
+    /*     :restart */
+    /*     # gotta get me some DHCP */
+    /*     dhcp */
+    /*     echo DEVICE SETTINGS.................... */
+    /*     echo mac..........$${mac} */
+    /*     echo busloc.......$${busloc} */
+    /*     echo bustype......$${bustype} */
+    /*     echo chip.........$${chiptype} */
+    /*     echo HOST SETTINGS...................... */
+    /*     echo hostname.....$${hostname} */
+    /*     echo uuid.........$${uuid} */
+    /*     echo user-class...$${user-class} */
+    /*     echo manufacturer.$${manufacturer} */
+    /*     echo product......$${product} */
+    /*     echo serial.......$${serial} */
+    /*     echo NETWORK SETTINGS................... */
+    /*     echo ip...........$${ip} */
+    /*     echo netmask......$${netmask} */
+    /*     echo gateway......$${gateway} */
+    /*     echo dns..........$${dns} */
+    /*     echo domain.......$${domain} */
+    /*     echo BOOT SETTINGS...................... */
+    /*     echo filename.....$${netX/filename} */
+    /*     echo next-server..$${netX/next-server} */
+    /*     echo default-url..$${netX/default-url} */
+    /*     echo MISC SETTINGS...................... */
+    /*     echo buildarch....$${buildarch} */
+    /*     echo platform.....$${platform} */
+
+    /*     # log info via GET that returns empty 200 OK */
+    /*     imgfetch http://freeside.skunkwerks.at/pub/ipxe/log?uuid=$${uuid}&mac=$${mac:hexhyp}&domain=$${domain}&hostname=$${hostname}&serial=$${serial}&platform=$${platform}&buildarch=$${buildarch}           || */
+    /*     imgfree */
+    /*     # if there is an ipxe script for this system use that */
+    /*     # else fall through increasingly generic scripts */
+    /*     chain http://freeside.skunkwerks.at/pub/ipxe/$${uuid}             || */
+    /*     chain http://freeside.skunkwerks.at/pub/ipxe/$${serial}           || */
+    /*     chain http://freeside.skunkwerks.at/pub/ipxe/$${mac}              || */
+    /*     chain http://freeside.skunkwerks.at/pub/ipxe/$${hostname}         || */
+    /*     chain http://freeside.skunkwerks.at/pub/ipxe/$${netX/default-url} || */
+
+    /*     :prompt */
+    /*     imgfetch http://freeside.skunkwerks.at/pub/$${platform}-$${buildarch}/boot/loader.efi || */
+    /*     prompt --key 0x1b --timeout 5000 Press ESC for iPXE shell... && shell || */
+    /*     boot loader.efi || */
+    /*     :restart */
+    /*     chain http://freeside.skunkwerks.at/pub/ipxe/ipxe.sh */
+/* IPXE */
 }
+
+
+resource "oci_core_instance_console_connection" "bilbo_console" {
+  instance_id = oci_core_instance.bilbo.id
+  public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8OZGfPONqBUYPOVaxmthl6YEfUfUrTppvBLd1m0AotOXUfNIp43foqMwoQ6SJ1fSAsaqXjPydpV1djvxlgh55B8xZoPvT188EefbDYLSHl1FTH/uufiIkRReT5xx4kqg8zhrOR8+kk2ICtwIhZn4SHpJaImutzOfMexFj/H6BPBjiVsWcwj7rlDDEn9Bx1lJ30d/1B7Qlaw5bySTs973BxKAMeny1tOBFOwnOqo93WpZ1dt8GJB+zq763Q2jPqZv3yQUxxPFGUSTzvmrx/WMkaqKW7IKeXZV9oxc8UYCBzU2li+uPMJCbAUpXk50pxNa2Kb2bQBwgEF3LawmfXFkN oracle_rsa"
+}
+
