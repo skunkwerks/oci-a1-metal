ac11edc1cb177678b007c4414fa78a9f76ee7882
Dave Cottlehuber <dch@skunkwerks.at>
2022-04-05 13:01:55 +0000

    first step in moving to tfvars

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
+locals {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaobmlhruktcqx2vi7r5mdu7eqkzucnjhziwfeb6s6thzgay4ujuhq"
+  compartment_ocid = "ocid1.tenancy.oc1..aaaaaaaaobmlhruktcqx2vi7r5mdu7eqkzucnjhziwfeb6s6thzgay4ujuhq"
+  availability_domain = "Ynwz:eu-amsterdam-1-AD-1"
+  subnet_ocid = "ocid1.subnet.oc1.eu-amsterdam-1.aaaaaaaa7lfgj4fcyfxlqc7cr4nhottzufvo5zzj2vc4syayk23i4rchenfq"
+  image_ocid = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaalnttofwyncslnxc5a757mb72w6dqkmiel4mlt6uoextxxgsu6ypa"
+  vm_memory = "12"
+  vm_ocpus = "2"
+  console_ssh_public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8OZGfPONqBUYPOVaxmthl6YEfUfUrTppvBLd1m0AotOXUfNIp43foqMwoQ6SJ1fSAsaqXjPydpV1djvxlgh55B8xZoPvT188EefbDYLSHl1FTH/uufiIkRReT5xx4kqg8zhrOR8+kk2ICtwIhZn4SHpJaImutzOfMexFj/H6BPBjiVsWcwj7rlDDEn9Bx1lJ30d/1B7Qlaw5bySTs973BxKAMeny1tOBFOwnOqo93WpZ1dt8GJB+zq763Q2jPqZv3yQUxxPFGUSTzvmrx/WMkaqKW7IKeXZV9oxc8UYCBzU2li+uPMJCbAUpXk50pxNa2Kb2bQBwgEF3LawmfXFkN oracle_rsa"
+  ssh_authorized_keys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHuuKqa0KEiCFC3Pr5LmWae/ZZfxOQcH9b9jFHLEMC3t ansible@cabal5.net 20201223\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ0cNlRkFRRleUZhFjIZYJ2p7h7wNWvODGBLEzfSfvr dch@skunkwerks.at 20201124\necdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBItIwUr8zhXOBtFH1B0YmNz2WJcY6w1ysRiTAIkI2CBenMb0f7H2pH1rFAa6ZF6dYS3SuLMng+igZUfkqhV/0Km+zus3lAjc37FFiawtATt+/nRj3hj/AaVz/cK7NnWdlg== dch@yk10616337_eccp384_20191024"
+
+}
+
 provider "oci" {}
 
-resource "oci_core_instance" "hobbes" {
+resource "oci_core_instance" "beastie" {
   agent_config {
     is_management_disabled = "true"
     is_monitoring_disabled = "true"
@@ -28,50 +41,50 @@ resource "oci_core_instance" "hobbes" {
   availability_config {
     recovery_action = "RESTORE_INSTANCE"
   }
-  availability_domain = "Ynwz:eu-amsterdam-1-AD-1"
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaaobmlhruktcqx2vi7r5mdu7eqkzucnjhziwfeb6s6thzgay4ujuhq"
+  availability_domain = local.availability_domain
+  compartment_id = local.compartment_ocid
   create_vnic_details {
     assign_private_dns_record = "true"
     assign_public_ip = "true"
-    subnet_id = "ocid1.subnet.oc1.eu-amsterdam-1.aaaaaaaa7lfgj4fcyfxlqc7cr4nhottzufvo5zzj2vc4syayk23i4rchenfq"
+    subnet_id = local.subnet_ocid
   }
-  display_name = "hobbes.cabal5.net"
+  display_name = "beastie"
   instance_options {
     are_legacy_imds_endpoints_disabled = "false"
   }
   metadata = {
-    "ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHuuKqa0KEiCFC3Pr5LmWae/ZZfxOQcH9b9jFHLEMC3t ansible@cabal5.net 20201223\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ0cNlRkFRRleUZhFjIZYJ2p7h7wNWvODGBLEzfSfvr dch@skunkwerks.at 20201124\necdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBItIwUr8zhXOBtFH1B0YmNz2WJcY6w1ysRiTAIkI2CBenMb0f7H2pH1rFAa6ZF6dYS3SuLMng+igZUfkqhV/0Km+zus3lAjc37FFiawtATt+/nRj3hj/AaVz/cK7NnWdlg== dch@yk10616337_eccp384_20191024"
+    "ssh_authorized_keys" = local.ssh_authorized_keys
   }
   shape = "VM.Standard.A1.Flex"
   shape_config {
-    memory_in_gbs = "24"
-    ocpus = "4"
+    memory_in_gbs = local.vm_memory
+    ocpus = local.vm_ocpus
   }
   source_details {
     source_type = "image"
-    source_id = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaalnttofwyncslnxc5a757mb72w6dqkmiel4mlt6uoextxxgsu6ypa"
+    source_id = local.image_ocid
   }
 }
 
-resource "oci_core_instance_console_connection" "hobbes_console" {
-  instance_id = oci_core_instance.hobbes.id
-  public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8OZGfPONqBUYPOVaxmthl6YEfUfUrTppvBLd1m0AotOXUfNIp43foqMwoQ6SJ1fSAsaqXjPydpV1djvxlgh55B8xZoPvT188EefbDYLSHl1FTH/uufiIkRReT5xx4kqg8zhrOR8+kk2ICtwIhZn4SHpJaImutzOfMexFj/H6BPBjiVsWcwj7rlDDEn9Bx1lJ30d/1B7Qlaw5bySTs973BxKAMeny1tOBFOwnOqo93WpZ1dt8GJB+zq763Q2jPqZv3yQUxxPFGUSTzvmrx/WMkaqKW7IKeXZV9oxc8UYCBzU2li+uPMJCbAUpXk50pxNa2Kb2bQBwgEF3LawmfXFkN oracle_rsa"
+resource "oci_core_instance_console_connection" "beastie_console" {
+  instance_id = oci_core_instance.beastie.id
+  public_key = local.console_ssh_public_key
 }
 
 
 output "oid" {
-  value = oci_core_instance.hobbes.id
+  value = oci_core_instance.beastie.id
 }
 
 output "ssh" {
-  value = oci_core_instance_console_connection.hobbes_console.connection_string
+  value = oci_core_instance_console_connection.beastie_console.connection_string
 }
 
 output "public_ip" {
-  value = oci_core_instance.hobbes.public_ip
+  value = oci_core_instance.beastie.public_ip
 }
 
 output "private_ip" {
-  value = oci_core_instance.hobbes.private_ip
+  value = oci_core_instance.beastie.private_ip
 }
 
